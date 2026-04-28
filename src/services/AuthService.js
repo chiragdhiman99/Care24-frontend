0048faaea912327be69c5ba5b6fd57abf179ce25
Chirag Dhiman <dhimanchirag99@gmail.com>
2026-04-28 23:12:36 +0530

    done some changes

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -7,7 +7,7 @@ const api = axios.create({
   withCredentials: true,
 });
 
-app.use.interceptors.request.use((config) => {
+api.use.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
